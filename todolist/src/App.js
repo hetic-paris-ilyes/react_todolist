3e12207c924664a9270f4eaeaeae7c247c9fa61a
ilyesb36 <ilyesb36130@gmail.com>
2022-03-16 13:56:45 +0100

    Setup react app

---
+++ todolist/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Title from './Component/react_title';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Title/>
+    </div>
+  );
+}
+
+export default App;
